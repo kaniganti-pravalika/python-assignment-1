f4ae6f5e3017b4ae63946b7c36d59be10bdffaa2
kaniganti-pravalika <pravalikak355@gmail.com>
2026-02-18 20:01:48 +0530

    new files added

---
+++ assign-5/counto-of-pairs.py
@@ -0,0 +1,23 @@
+'''1.count pairs whose sum is less than the target
+
+
+Given a 0-indexed integer array nums of length n and an integer target, return the number of pairs (i, j) where 0 <= i < j < n and nums[i] + nums[j] < target.
+ 
+Example 1:
+Input: nums = [-1,1,2,3,1], target = 2
+Output: 3
+Explanation: There are 3 pairs of indices that satisfy the conditions in the statement:
+- (0, 1) since 0 < 1 and nums[0] + nums[1] = 0 < target
+- (0, 2) since 0 < 2 and nums[0] + nums[2] = 1 < target 
+- (0, 4) since 0 < 4 and nums[0] + nums[4] = 0 < target
+Note that (0, 3) is not counted since nums[0] + nums[3] is not strictly less than the target.
+
+'''
+class Solution:
+    def countPairs(self, nums, target):
+        count=0
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[j]+nums[i]<target:
+                    count=count+1
+        return count
